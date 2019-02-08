f4091757382984e2c7c4795de8e82f3e399e3fbe
nejmal <lam.jennifer.ky@gmail.com>
2019-02-08 13:31:46 -0800

    Add grids to welcome pg.

--- imports/containers/App/index.js
+++ imports/containers/App/index.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Layout from "../../routes/Layout";
-import { Clients } from "../../api/clients";
-import { withTracker } from "meteor/react-meteor-data";
-import AccountsUIWrapper from "../../ui/components//AccountsWrapper/index";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Layout from '../../routes/Layout';
+import { Clients } from '../../api/clients';
+import { withTracker } from 'meteor/react-meteor-data';
+// import AccountsUIWrapper from '../../ui/components//AccountsWrapper/index';
 
 ///// DO NOT TOUCH
 // import logo from '../../public/images/welcome.jpg';
-import "./styles.css";
-import theme from "../../theme";
+import './styles.css';
+import theme from '../../theme';
 
 class App extends Component {
   render() {
-    console.log("from the");
+    console.log('from the');
     console.log(this.props);
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app-wrapper">
-          <div className="login-wrapper">
+        <div className='app-wrapper'>
+          {/* <div className="login-wrapper">
             <AccountsUIWrapper />
-          </div>
-          <div className="container">
+          </div> */}
+          <div className='container'>
             <header>{/* <img src={logo} alt='logo' /> */}</header>
             <h1>
               {this.props.clients.map(a => (
-                <h1>{a.name}</h1>
+                <h1 key={a._id}>{a.name}</h1>
               ))}
             </h1>
             <Layout />
--- imports/ui/pages/Welcome/Welcome.js
+++ imports/ui/pages/Welcome/Welcome.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import WelcomeBg from '../../../images/welcome.jpg';
-import styles from "./styles";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import AccountsUIWrapper from '../../components/AccountsUIWrapper/';
+import styles from './styles';
 
 const Welcome = ({ classes }) => {
-  return <div>This is the Welcome page</div>;
+  return (
+    <Grid
+      container
+      className={classes.root}
+      direction='row'
+      alignItems='center'
+      justify='center'
+    >
+      <Grid item xs={12} sm={12} md={6}>
+        {/* <Typography
+          variant='button'
+          gutterBottom
+          className={classes.subheading}
+        >
+          Boomtown
+        </Typography> */}
+        <Typography variant='display4' className={classes.headline}>
+          Left Side
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} className={classes.loginWrapper}>
+        {/* <Typography gutterBottom variant='headline'>
+          Welcome home.
+        </Typography> */}
+        {/* <div className={classes.loginWrapper}> */}
+        <AccountsUIWrapper />
+        <Typography variant='display4' className={classes.headline}>
+          Right Side
+        </Typography>
+        {/* </div> */}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Welcome;
