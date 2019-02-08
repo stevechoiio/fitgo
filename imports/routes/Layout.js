7285fdbd5d70a6500623d21af45d7987ce76a416
Steve Choi <stevechoi93@gmail.com>
2019-02-08 11:26:36 -0800

    changed lowercase file issue

--- imports/routes/Layout.js
+++ imports/routes/Layout.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import Profile from '../ui/pages/Profile/';
-import Welcome from '../ui/pages/Welcome';
-
-import Feature from '../ui/pages/Feature';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-=======
 import React from "react";
-
 import Profile from "../ui/pages/Profile/";
-
 import Welcome from "../ui/pages/Welcome";
 
 import Feature from "../ui/pages/Feature";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
->>>>>>> bf47eea7fad200134795841be652b07c936cc889
 
 ///DO NOT ADD
 
@@ -22,10 +11,10 @@ const Layout = () => {
   return (
     <Router>
       <div>
-        <Route exact path='/' component={Welcome} />
+        <Route exact path="/" component={Welcome} />
         {/* <Route exact path='/chat' component={Chat} /> */}
-        <Route exact path='/feature' component={Feature} />
-        <Route exact path='/profile' component={Profile} />
+        <Route exact path="/feature" component={Feature} />
+        <Route exact path="/profile" component={Profile} />
       </div>
     </Router>
   );
--- imports/ui/pages/Feature/Feature.js
+++ imports/ui/pages/Feature/Feature.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import GoogleMaps from '../../../ui/components//Map';
-
-import styles from './styles';
-=======
 import React from "react";
-
 import GoogleMaps from "../../../ui/components//Map";
 
 import styles from "./styles";
->>>>>>> bf47eea7fad200134795841be652b07c936cc889
 
 const Feature = () => {
   return (
