f242670e2a9c7df5d70b64c11fe91863b3c604aa
Steve Choi <stevechoi93@gmail.com>
2019-02-08 10:50:13 -0800

    layout.js

--- imports/routes/Layout.js
+++ imports/routes/Layout.js
@@ -1,7 +1,12 @@
 import React from "react";
+<<<<<<< HEAD
 import Profile from "../ui/pages/Profile/";
 
 import Welcome from "../ui/pages/Welcome";
+=======
+import Profile from "../ui/pages/Profile";
+import Welcome from "../ui/pages/Welcome/Welcome";
+>>>>>>> cbb217ce3aa00dab576ede18be1e18c5235610b6
 import Feature from "../ui/pages/Feature";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
--- imports/ui/pages/Feature/Feature.js
+++ imports/ui/pages/Feature/Feature.js
@@ -1,5 +1,9 @@
 import React from "react";
+<<<<<<< HEAD
 import GoogleMaps from "../../../components/Map";
+=======
+import GoogleMaps from "../../../ui/components//Map";
+>>>>>>> cbb217ce3aa00dab576ede18be1e18c5235610b6
 import styles from "./styles";
 
 const Feature = () => {
