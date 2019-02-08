321346f0a81a8f5a52a5ebea0fb74fc8ffff7135
Timmy <timgabrielnguyen@gmail.com>
2019-02-08 13:43:47 -0800

    Add Publish and Subscription for Server and Client

--- imports/api/clients.js
+++ imports/api/clients.js
@@ -1,3 +1,10 @@
 import { Mongo } from "meteor/mongo";
+import Meteor from "meteor/meteor";
+
+if (Meteor.isServer) {
+  Meteor.publish("clients", function clientsPublication() {
+    return Clients.find({ owner: this.userId });
+  });
+}
 
 export const Clients = new Mongo.Collection("clients");
