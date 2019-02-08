21e4e86aa5f21f776b78047d9473402ae5de37a2
Steve Choi <stevechoi93@gmail.com>
2019-02-08 10:28:28 -0800

    file change

--- imports/ui/pages/Feature/Feature.js
+++ imports/ui/pages/Feature/Feature.js
@@ -1,13 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import GoogleMaps from "../../../components/Map";
-=======
-import Map from "../../../ui/components//Map";
->>>>>>> newFile
-=======
-import Map from "../../../ui/components//Map";
->>>>>>> newFile
 import styles from "./styles";
 
 const Feature = () => {
