5b2f8fd607cc9f1c1caca1b8266f6868c6b8ac1e
nejmal <lam.jennifer.ky@gmail.com>
2019-02-08 09:29:40 -0800

    Change chat.

--- imports/components/Chat/chat.js
+++ imports/components/Chat/chat.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './styles';
 
 const Chat = () => {
-  return <div>This is the Chat page</div>;
+  return <div>This is a Chat</div>;
 };
 
 export default Chat;
