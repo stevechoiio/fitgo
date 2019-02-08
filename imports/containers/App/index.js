b33e04ed4dc750cac0227ea33841dc30e21f25bc
nejmal <lam.jennifer.ky@gmail.com>
2019-02-08 08:11:04 -0800

    Add MuiThemeProvider and CssBaseline.

--- imports/containers/App/index.js
+++ imports/containers/App/index.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Layout from '../../routes/Layout';
 import { Clients } from '../../api/clients';
 import { withTracker } from 'meteor/react-meteor-data';
 // import logo from '../../public/images/welcome.jpg';
+import './styles.css';
+import theme from '../../theme';
 
 class App extends Component {
   render() {
     console.log('from the');
     console.log(this.props);
     return (
-      <div className='container'>
-        <header>{/* <img src={logo} alt='logo' /> */}</header>
-        <h1>
-          {this.props.clients.map(a => (
-            <h1>{a.name}</h1>
-          ))}
-        </h1>
-        <Layout />
-        <h1 />
-        <ul />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='container'>
+          <header>{/* <img src={logo} alt='logo' /> */}</header>
+          <h1>
+            {this.props.clients.map(a => (
+              <h1>{a.name}</h1>
+            ))}
+          </h1>
+          <Layout />
+          <h1 />
+          <ul />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
