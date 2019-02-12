677afa58d36e6b95994fc4c575580d56e3a8b431
Steve Choi <stevechoi93@gmail.com>
2019-02-12 11:04:21 -0800

    added tagfilter function

--- imports/ui/components/MapWithAMarker/MapWithAMarker.js
+++ imports/ui/components/MapWithAMarker/MapWithAMarker.js
@@ -9,22 +9,7 @@ import {
 import distanceFilter from "./DistanceCalculator";
 import GoogleMapStyles from "./GoogleMapStyles.json";
 import OptionBar from "../OptionBar/index";
-
-const LocationListOfTrainers = [
-  { latitude: 49.008712, longitude: -122.751125 },
-  { latitude: 49.008712, longitude: -123.751125 },
-  { latitude: 49.008712, longitude: -121.751125 },
-  { latitude: 48.028712, longitude: -122.751125 },
-  { latitude: 49.044233, longitude: -123.751125 },
-  { latitude: 59.005112, longitude: -121.751125 },
-  { latitude: 41.008712, longitude: -123.123425 },
-  { latitude: 46.053212, longitude: -125.751125 },
-  { latitude: 45.006612, longitude: -114.756665 },
-  { latitude: 48.003432, longitude: -133.751925 },
-  { latitude: 49.192222, longitude: -113.751125 },
-  { latitude: 55.666412, longitude: -121.751555 },
-  { latitude: 49.209017, longitude: -122.842986 }
-];
+import { LocationListOfTrainers } from "./fakeData";
 
 class MapWithAMarker extends React.Component {
   constructor(props) {
@@ -71,7 +56,6 @@ class MapWithAMarker extends React.Component {
     }
   };
   render() {
-    console.log(this.props);
     return (
       <Fragment>
         <GoogleMap
