1a0a576fb41bb274372d2b58e959df3f6ba1cf78
nejmal <lam.jennifer.ky@gmail.com>
2019-02-11 17:16:25 -0800

    Added grid container to profile.

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
-import styles from "./styles";
-import { Clients } from "../../../api/clients";
-import { Trainers } from "../../../api/trainers";
-import { withTracker } from "meteor/react-meteor-data";
+import React, { Component } from 'react';
+import { Clients } from '../../../api/clients';
+import { Trainers } from '../../../api/trainers';
+import { withTracker } from 'meteor/react-meteor-data';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
 
 class Profile extends Component {
   componentDidMount() {
@@ -19,24 +21,32 @@ class Profile extends Component {
     console.log(trainers);
 
     return (
-      <div className="profile">
-        {trainers.map(trainer => (
-          <div key={trainer._id}>
-            <h1>Name: {trainer.name}</h1>
-            <h2>Email: {trainer.email}</h2>
-            <h2>Education: {trainer.education}</h2>
-            <h2>Languages: {trainer.languages.join(", ")}</h2>
-            <h2>Skills: {trainer.skills.join(", ")}</h2>
-          </div>
-        ))}
-      </div>
+      <Grid
+        container
+        className={classes.root}
+        direction='row'
+        alignItems='center'
+        justify='center'
+      >
+        <div className={classes.profile}>
+          {trainers.map(trainer => (
+            <div key={trainer._id}>
+              <h1>Name: {trainer.name}</h1>
+              <h2>Email: {trainer.email}</h2>
+              <h2>Education: {trainer.education}</h2>
+              <h2>Languages: {trainer.languages.join(', ')}</h2>
+              <h2>Skills: {trainer.skills.join(', ')}</h2>
+            </div>
+          ))}
+        </div>
+      </Grid>
     );
   }
 }
 
 export default withTracker(() => {
-  Meteor.subscribe("clients"); // NEW!
-  Meteor.subscribe("trainers");
+  Meteor.subscribe('clients'); // NEW!
+  Meteor.subscribe('trainers');
   console.log(Meteor.user());
   return {
     trainers: Trainers.find({}).fetch(),
@@ -44,4 +54,4 @@ export default withTracker(() => {
     currentUserId: Meteor.userId(),
     clients: Clients.find({}).fetch()
   };
-})(Profile);
+})(withStyles(styles)(Profile));
--- imports/ui/pages/Profile/styles.js
+++ imports/ui/pages/Profile/styles.js
@@ -1,7 +1,22 @@
 const styles = theme => ({
-    profile: {
-        color: 'red'
+  root: {
+    backgroundImage: "url('/profileH.jpg')",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    // filter: 'grayscale(100%)',
+    height: '100vh',
+    flexGrow: 1,
+    background: theme.palette.primary.main,
+    padding: theme.spacing.unit * 5,
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing.unit * 20
     }
+  },
+  profile: {
+    color: 'red',
+    background: 'white'
+  }
 });
 
 export default styles;
