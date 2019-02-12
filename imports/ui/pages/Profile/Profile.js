895fcd8a2d9546583964af7ac86d60ceb6d696cf
Timmy <timgabrielnguyen@gmail.com>
2019-02-11 16:26:09 -0800

    Filter Out Client on Profie Page

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -5,20 +5,30 @@ import { Trainers } from "../../../api/trainers";
 import { withTracker } from "meteor/react-meteor-data";
 
 class Profile extends Component {
+  componentDidMount() {
+    // const trainers = this.props.trainers.filter((trainer) => trainer.username === currentUser.username)
+    // console.log(trainers);
+  }
+
   render() {
-    const { classes, clients } = this.props;
+    const { classes, clients, currentUser } = this.props;
+
+    const trainers = this.props.trainers.filter(
+      trainer => trainer.username === currentUser.username
+    );
+    console.log(trainers);
 
     return (
       <div className="profile">
-           {clients.map(client => (
-             <div key={client._id}>
-             <h1>Name: {client.name}</h1>
-            <h2>Email: {client.email}</h2>
-            <h2>Education: {client.education}</h2>
-            <h2>Languages: {client.languages.join(", ")}</h2>
-            <h2>Skills: {client.skills.join(", ")}</h2>
-             </div>
-            ))}
+        {trainers.map(trainer => (
+          <div key={trainer._id}>
+            <h1>Name: {trainer.name}</h1>
+            <h2>Email: {trainer.email}</h2>
+            <h2>Education: {trainer.education}</h2>
+            <h2>Languages: {trainer.languages.join(", ")}</h2>
+            <h2>Skills: {trainer.skills.join(", ")}</h2>
+          </div>
+        ))}
       </div>
     );
   }
@@ -27,11 +37,11 @@ class Profile extends Component {
 export default withTracker(() => {
   Meteor.subscribe("clients"); // NEW!
   Meteor.subscribe("trainers");
-
+  console.log(Meteor.user());
   return {
-    trainers: Trainers.find({user: Meteor.username}).fetch(),
+    trainers: Trainers.find({}).fetch(),
     currentUser: Meteor.user(),
     currentUserId: Meteor.userId(),
-    clients: Clients.find({user: Meteor.username}).fetch()
+    clients: Clients.find({}).fetch()
   };
 })(Profile);
