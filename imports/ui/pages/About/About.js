ad62440fdf9fd7e345064e21f9a1859d4b6d0f6b
nejmal <lam.jennifer.ky@gmail.com>
2019-02-13 23:31:35 -0800

    Add phone icon to profile.

--- imports/ui/pages/About/About.js
+++ imports/ui/pages/About/About.js
@@ -1,14 +1,10 @@
-import React, { Component } from "react";
-import styles from "./styles";
-import OptionList from "../../components/OptionsList";
-import FullScreenLoader from "../../components/FullScreenLoader";
+import React, { Component } from 'react';
+import styles from './styles';
+import OptionList from '../../components/OptionsList';
+import FullScreenLoader from '../../components/FullScreenLoader';
 
 const About = ({ classes }) => {
-  return (
-    <div>
-      <OptionList />
-    </div>
-  );
+  return <div>This is the about page</div>;
 };
 
 export default About;
