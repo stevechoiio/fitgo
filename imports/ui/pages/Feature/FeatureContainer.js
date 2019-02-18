dc9680d091c5837b56fb2e47fe54d36bbef548a2
Steve Choi <stevechoi93@gmail.com>
2019-02-18 15:20:33 -0800

    container files deleted

--- imports/ui/pages/Feature/FeatureContainer.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import Feature from "./Feature";
-// import { withStyles } from "@material-ui/core/styles";
-// import styles from "./styles";
-
-// const FeatureContainer = ({ classes }) => <Feature classes={classes} />;
-
-// export default withStyles(styles)(FeatureContainer);
