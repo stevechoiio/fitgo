309c7f8fc03c38730b5b9d352264fa4f1e5d2c35
Steve Choi <stevechoi93@gmail.com>
2019-02-18 14:55:52 -0800

    removed unused files in MapWithAMarker Folder

--- imports/ui/components/MapWithAMarker/skillsFilter.js
+++
@@ -1,9 +0,0 @@
-import { LocationListOfTrainers } from "./fakeData";
-
-const skillsFilter = (selectedTags, fakeData) => {
-  fakeData.filter(trainer => {
-    trainer.skills.some(skill => selectedTags.includes(skill));
-  });
-};
-
-console.log(skillsFilter(["yoga"], LocationListOfTrainers));
